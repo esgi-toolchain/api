95f2aaa94e6cc09c4574fdc264b36078c4a5b95e
Luis Valdez <luis.valdez@affluences.com>
2020-02-14 09:09:07 +0100

    express

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,23 @@
-import './trace/ddTracer';
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 
-// const ddOptions = {
-//   path: true,
-//   base_url: true,
-//   method: true,
-//   protocol: true,
-//   response_code: true,
-//   tags: ['env:prod'],
-// };
-//
-// const connectDatadog = require('connect-datadog')(ddOptions);
+const ddOptions = {
+  path: true,
+  base_url: true,
+  method: true,
+  protocol: true,
+  response_code: true,
+  tags: ['env:prod'],
+};
+
+const connectDatadog = require('connect-datadog')(ddOptions);
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({logger: true}));
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
-  // app.use(connectDatadog);
+  app.use(connectDatadog);
   await app.listen(parseInt(process.env.PORT, 10) || 3000, '0.0.0.0');
 }
 bootstrap();
